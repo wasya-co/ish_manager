7ed3bacdead66b5fd78883ffcf55cbfc6f4fcb0c
piousbox <piousbox@gmail.com>
2017-05-25 15:12:58 -0500

    0.1.3, assets should render

--- lib/ish_manager/engine.rb
+++ lib/ish_manager/engine.rb
@@ -1,8 +1,16 @@
 module IshManager
   class Engine < ::Rails::Engine
     isolate_namespace IshManager
-    initializer "blorgh.assets.precompile" do |app|
-      app.config.assets.precompile += %w( application.js application.css )
+
+=begin
+    initializer :assets do |config|
+      # Rails.application.config.assets.precompile += %w{ ish_manager/application.js ish_manager/application.css application.js application.css }
+      # Rails.application.config.assets.paths << root.join("ish_manager", "app", "assets", "images")
+    end
+=end
+
+    initializer "ish_manager.assets.precompile" do |app|
+      app.config.assets.precompile += %w( ish_manager/application.js ish_manager/application.css )
     end
   end
 end
