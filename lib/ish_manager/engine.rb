f7aa2f107dbcc666e318a88d7a9461c29c6608e9
piousbox <piousbox@gmail.com>
2017-05-25 14:48:21 -0500

    working on compiling assets

--- lib/ish_manager/engine.rb
+++ lib/ish_manager/engine.rb
@@ -1,5 +1,8 @@
 module IshManager
   class Engine < ::Rails::Engine
     isolate_namespace IshManager
+    initializer "blorgh.assets.precompile" do |app|
+      app.config.assets.precompile += %w( application.js application.css )
+    end
   end
 end
