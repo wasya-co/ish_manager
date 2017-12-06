da5e2eb764dbc0e66e82e477c14625723e810693
piousbox <piousbox@gmail.com>
2017-12-05 23:57:54 -0800

    autobuild `date "+%Y%m%d"`

--- app/controllers/ish_manager/galleries_controller.rb
+++ app/controllers/ish_manager/galleries_controller.rb
@@ -66,6 +66,7 @@ class IshManager::GalleriesController < IshManager::ApplicationController
     authorize! :update, @gallery
     params[:gallery][:shared_profiles].delete('')
     params[:gallery][:shared_profiles] = IshModels::UserProfile.find params[:gallery][:shared_profiles]
+    puts! params[:gallery][:shared_profiles], 'shared profiles'
     if @gallery.update_attributes( params[:gallery].permit! )
       new_shared_profiles = params[:gallery][:shared_profiles].select { |p| !old_shared_profile_ids.include?( p.id ) }
       ::IshManager::ApplicationMailer.shared_galleries( new_shared_profiles, @gallery ).deliver
