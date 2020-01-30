174dd5949856cb34196dc2f47362965376d3e3e2
Victor Pudeyev <piousbox@gmail.com>
2020-01-29 18:56:49 -0600

    improved login flow in tgm

--- app/models/ish_manager/ability.rb
+++ app/models/ish_manager/ability.rb
@@ -9,40 +9,42 @@ class IshManager::Ability
     #
     unless user.blank?
       
+      #
+      # only sudoer... total power
+      #
+      if user.profile && user.profile.sudoer?
+        can :manage, :all
+      end
+
       can [ :home ], ::IshManager::Ability
       
       can [ :friends_index, :friends_new ], ::IshModels::UserProfile
 
       #
-      # role manager
+      # role admin, manager
       #
       if user.profile && [ :admin, :manager ].include?( user.profile.role_name )    
 
         can [ :create_newsitem, :show, :new_feature, :create_feature ], ::City
+        can [ :manage ], ::CoTailors
 
         can [ :new ], ::Feature
 
-        can [ :manage ], ::CoTailors
+        can [ :index ], ::Gallery
+        
         can [ :cities_index, :home, :sites_index, :venues_index ], ::Manager
 
         can [ :new ], Newsitem
 
         can [ :new, :create ], Report
 
-        can [ :show, :edit, :update, :create_newsitem, :new_feature, :create_feature ], ::Site do |site|
+        can [ :show, :edit, :update, :create_newsitem, :new_feature, :create_feature, :newsitems_index ], ::Site do |site|
           !site.is_private && !site.is_trash
         end
         can [ :manage ], Ish::StockWatch
 
         # can [ :new_feature, :create_feature ], ::Tag
 
-      end
-
-      #
-      # only sudoer... total power
-      #
-      if user.profile && user.profile.sudoer?
-        can :manage, :all
       end
 
       #
