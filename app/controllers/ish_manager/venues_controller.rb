2932d0bae5a718d9ffdb8a1c010202ee8f9f16c2
Adil Abbas <adil.abbas@activekeysolutions.com>
2020-03-25 20:52:56 +0500

    issue 463 new fixes.

--- app/controllers/ish_manager/venues_controller.rb
+++ app/controllers/ish_manager/venues_controller.rb
@@ -15,36 +15,26 @@ class IshManager::VenuesController < IshManager::ApplicationController
   def new
     @venue = Venue.new
     @venue.city_id = params[:city_id] if params[:city_id]
-    @venue.to_city = 'yes' if params[:city_id]
-
     authorize! :new, @venue
   end
 
   def create
-    redirect_path = venues_path()
     @venue = Venue.new params[:venue].permit!
     authorize! :create, @venue
-    if params[:venue][:to_city] == 'yes'
-      redirect_path = city_path( params[:venue][:city_id] )
-    end
     if @venue.save
-      redirect_to redirect_path, :notice => 'Venue successfully created.'
+      redirect_to city_path( params[:venue][:city_id] ), :notice => 'Venue successfully created.'
     else
-      #flash[:alert] = @venue.errors.messages
+      flash[:alert] = @venue.errors.messages
       render :action => :new
     end
   end
 
   def edit
     @venue = Venue.find params[:id]
-    @venue.to_city = params[:from_city]
     authorize! :edit, @venue
   end
 
   def update
-
-    redirect_path = venues_path()
-    redirect_path = city_path( params[:venue][:city_id] ) if params[:venue][:to_city] == 'yes'
     params[:venue][:tag_ids].delete ''
     @resource = @venue = Venue.find params[:id]
     authorize! :update, @venue
@@ -52,7 +42,7 @@ class IshManager::VenuesController < IshManager::ApplicationController
     flag = @venue.update_attributes params[:venue].permit!
     if flag
       flash[:notice] = 'updated venue'
-      redirect_to redirect_path
+      redirect_to city_path( params[:venue][:city_id] )
     else
       flash[:alert] = "No luck: #{@venue.errors.messages}"
       render :action => :edit
@@ -69,14 +59,12 @@ class IshManager::VenuesController < IshManager::ApplicationController
     @venue = Venue.unscoped.find params[:id]
     venue_name = @venue.name
     authorize! :delete, @venue
-    redirect_path = venues_path
-    redirect_path = city_path( @venue.city_id ) if params[:from_city] == 'yes'
     if @venue.destroy
       flash[:notice] = "Deleted venue #{venue_name}"
     else
       flash[:alert] = "Cannot delete venue: #{@venue.errors.messages}"
     end
-    redirect_to redirect_path
+    redirect_to city_path( @venue.city_id )
   end
 
 end
