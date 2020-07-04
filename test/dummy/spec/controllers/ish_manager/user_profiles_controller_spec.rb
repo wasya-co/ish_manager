9cd514bc3cda225768c0917616fb447f2e2cd2c8
Victor Pudeyev <victor.pudeyev@haulhub.com>
2020-07-04 16:51:11 -0500

    no about in profiles

--- test/dummy/spec/controllers/ish_manager/user_profiles_controller_spec.rb
+++ test/dummy/spec/controllers/ish_manager/user_profiles_controller_spec.rb
@@ -4,16 +4,21 @@ describe IshManager::UserProfilesController, :type => :controller do
   render_views
   routes { IshManager::Engine.routes }
 
-  before :each do
+  before do
     User.all.destroy
     @user = FactoryGirl.create :user
     sign_in @user, :scope => :user
 
     allow(controller).to receive(:current_user).and_return(UserStub.new({ :manager => true }))
 
-    @profile = IshModels::UserProfile.new :user => @user, :email => 'some@email.com', :name => 'some-name-2'
-    @profile.save
-    @profile.persisted?.should eql true
+    @profile = IshModels::UserProfile.create! :user => @user, :email => 'some@email.com', :name => 'some-name-2'
+  end
+
+  describe "#index" do
+    it "renders" do
+      get :index
+      response.should be_success
+    end
   end
 
   describe '#new' do
