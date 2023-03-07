1419825cf7898f32f46b0b5189c833fee0599656
mac_a2141 <victor@piousbox.com>
2023-03-07 17:57:34 -0500

    small email grok for intake job, lets deploy

--- app/controllers/ish_manager/email_filters_controller.rb
+++ app/controllers/ish_manager/email_filters_controller.rb
@@ -5,8 +5,42 @@ class ::IshManager::EmailFiltersController < ::IshManager::ApplicationController
 
   # alphabetized : )
 
+  def create
+    authorize! :create, Office::EmailFilter
+    @email_filter = Office::EmailFilter.create params[:email_filter].permit!
+    if @email_filter.persisted?
+      flash[:notice] = 'Success'
+    else
+      flash[:alert] = "No luck: #{@email_filter.errors.full_messages.join(', ')}."
+    end
+    redirect_to action: 'index'
+  end
+
+  def edit
+    @email_filter = Office::EmailFilter.find params[:id]
+    authorize! :edit, @email_filter
+  end
+
   def index
     authorize! :index, Office::EmailFilter.new
+    @email_filters = Office::EmailFilter.active
+  end
+
+  def new
+    @email_filter = Office::EmailFilter.new
+    authorize! :new, @email_filter
+  end
+
+  def update
+    @email_filter = Office::EmailFilter.find params[:id]
+    authorize! :update, @email_filter
+    @email_filter.update_attributes( params[:email_filter].permit! )
+    if @email_filter.persisted?
+      flash[:notice] = 'Success'
+    else
+      flash[:alert] = "No luck: #{@email_filter.errors.full_messages.join(', ')}."
+    end
+    redirect_to action: 'index'
   end
 
 end
