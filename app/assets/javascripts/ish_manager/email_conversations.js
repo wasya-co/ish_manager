34789981f952919659c8d518343b827d8faace19
mac_a2141 <victor@piousbox.com>
2023-03-15 18:57:36 -0500

    better view of an email msg

--- app/assets/javascripts/ish_manager/email_conversations.js
+++ app/assets/javascripts/ish_manager/email_conversations.js
@@ -5,29 +5,15 @@ $(document).ready(() => {
     // logg(e, 'clicked')
 
     if ($(this).data('expanded')) {
-      $(this).parent().find(".expand").html('')
-      $(this).parent().find(".my-actions").addClass('hide')
       $(this).data('expanded', false)
-      return
+      $(this).parent().find(".my-actions").addClass('hide')
+      $(this).parent().find("iframe").css('display', 'none')
+    } else {
+      $(this).data('expanded', true)
+      $(this).parent().find(".my-actions").removeClass('hide')
+      $(this).parent().find("iframe").css('display', 'block')
     }
 
-    const action_path = "/api/email_messages/" + $(this).data('id')
-
-    $.ajax({
-      url: action_path,
-      type: 'GET',
-      data: {
-        jwt_token: $( $("#Config")[0] ).data('jwt-token'),
-      },
-      success: e => {
-        $(this).parent().find(".expand").html(e.item.part_html)
-        $(this).parent().find(".my-actions").removeClass('hide')
-        $(this).data('expanded', true)
-      },
-      error: e => {
-        logg((e||{}).responseText, 'cannot get email_message')
-      },
-    })
   })
 
 })
