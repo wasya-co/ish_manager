52ba8d73f996795ae881e639d5a13e5f1636d00c
mac_a2141 <victor@piousbox.com>
2023-03-15 21:15:43 -0500

    autobuild `date "+%Y%m%d"`

--- app/controllers/ish_manager/email_conversations_controller.rb
+++ app/controllers/ish_manager/email_conversations_controller.rb
@@ -7,7 +7,7 @@ class ::IshManager::EmailConversationsController < IshManager::ApplicationContro
 
   def index
     authorize! :email_conversations_index, IshManager::Ability
-    @email_conversations = ::Office::EmailConversation.all.order_by( latest_at: :desc )
+    @email_conversations = ::Office::EmailConversation.in_inbox
   end
 
   def show
