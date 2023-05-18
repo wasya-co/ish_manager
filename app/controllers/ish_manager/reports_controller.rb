897b2e6821188021d2861c62af36d57e421be069
mac_a2141 <victor@piousbox.com>
2023-05-18 14:11:11 -0500

    autobuild `date "+%Y%m%d"`

--- app/controllers/ish_manager/reports_controller.rb
+++ app/controllers/ish_manager/reports_controller.rb
@@ -6,16 +6,16 @@ class IshManager::ReportsController < IshManager::ApplicationController
   # alphabetized : )
 
   def create
-    @report = Report.new params[:report].permit!
-    @report.user_profile = @current_profile # @TODO: this should not be hard-coded
-    authorize! :create, @report
+    authorize! :create, Report
 
-    old_shared_profile_ids = @report.shared_profiles.map(&:id)
     if params[:report][:shared_profiles].present?
       params[:report][:shared_profiles].delete('')
     end
     params[:report][:shared_profile_ids] = params[:report][:shared_profiles]
     params[:report].delete :shared_profiles
+
+    @report = Report.new params[:report].permit!
+    @report.user_profile = @current_profile # @TODO: this should not be hard-coded
 
     flag = @report.save
     respond_to do |format|
