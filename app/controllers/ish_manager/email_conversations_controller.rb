612a41c1c59ecb019da91d0f4ccb0caa6350d0fb
mac_a2141 <victor@piousbox.com>
2023-09-23 20:02:45 -0500

    autobuild `date "+%Y%m%d"`

--- app/controllers/ish_manager/email_conversations_controller.rb
+++ app/controllers/ish_manager/email_conversations_controller.rb
@@ -7,12 +7,12 @@ class ::IshManager::EmailConversationsController < IshManager::ApplicationContro
     authorize! :email_conversations_index, IshManager::Ability
     @email_conversations = ::Office::EmailConversation.all
 
-    if current_profile.per_page > 100
-      flash_notice "Cannot display more than 100 conversations per page."
-      per_page = 100
-    else
-      per_page = current_profile.per_page
-    end
+    # if current_profile.per_page > 100
+    #   flash_notice "Cannot display more than 100 conversations per page."
+    #   per_page = 100
+    # else
+    #   per_page = current_profile.per_page
+    # end
 
     if params[:slug]
       @email_conversations = @email_conversations.in_emailtag( params[:slug] )
