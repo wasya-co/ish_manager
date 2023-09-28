ba87a8e498f5f1ee0f98e189da91b854494da131
mac_a2141 <victor@piousbox.com>
2023-09-28 15:08:02 -0500

    autobuild `date "+%Y%m%d"`

--- app/controllers/ish_manager/unsubscribes_controller.rb
+++ app/controllers/ish_manager/unsubscribes_controller.rb
@@ -4,6 +4,9 @@ class ::IshManager::UnsubscribesController < IshManager::ApplicationController
   def index
     authorize! :index, Ish::EmailUnsubscribe
     @unsubscribes = Ish::EmailUnsubscribe.all
+    lead_ids = @unsubscribes.map(&:lead_id).compact
+    @leads_h = Lead.find_to_h( lead_ids )
+    puts! @leads_h, 'ddz'
   end
 
 end
