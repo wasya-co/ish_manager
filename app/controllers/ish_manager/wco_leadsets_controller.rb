98f6c981e9d4b87a2a8ae51e060dbe51d068f82e
mac_a2141 <victor@piousbox.com>
2023-11-02 17:22:23 -0300

    ok

--- app/controllers/ish_manager/wco_leadsets_controller.rb
+++ app/controllers/ish_manager/wco_leadsets_controller.rb
@@ -20,6 +20,15 @@ class IshManager::WcoLeadsetsController < IshManager::ApplicationController
   end
 
   def update
+    @wco_leadset = Wco::Leadset.find params[:id]
+    authorize! :update, @wco_leadset
+    flag = @wco_leadset.update params[:wco_leadset].permit!
+    if flag
+      flash_notice "ok"
+    else
+      flash_alert "sorry"
+    end
+    redirect_to action: :index
   end
 
 end
